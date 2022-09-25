af1420dc5c2f1d34785161dd4638c3c41e10854a
yuval <nhajeho1@gmail.com>
2022-09-25 13:44:39 +0300

    added sqlite

--- app.ts
+++ app.ts
@@ -3,6 +3,19 @@ import { getMako } from "./news-providers/mako";
 import { getWalla } from "./news-providers/walla";
 import { getYnet } from "./news-providers/ynet";
 
-// getYnet().then((data) => writeFileSync("ynet.json", JSON.stringify(data)));
-getMako().then((data) => writeFileSync("mako.json", JSON.stringify(data)));
+import Database from "better-sqlite3";
+const db = new Database("db.db");
+
+db.prepare(
+	`CREATE TABLE IF NOT EXISTS articles (
+    guid PRIMARY KEY,
+	title,
+   	link,
+    pubDate,
+    content,
+    contentSnippet
+)`
+).run();
+getYnet().then((data) => writeFileSync("ynet.json", JSON.stringify(data)));
+// getMako().then((data) => writeFileSync("mako.json", JSON.stringify(data)));
 // getWalla().then((data) => writeFileSync("walla.json", JSON.stringify(data)));
--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const mako_1 = require("./news-providers/mako");
+const better_sqlite3_1 = __importDefault(require("better-sqlite3"));
+const db = new better_sqlite3_1.default("db.db");
+db.prepare(`CREATE TABLE IF NOT EXISTS articles (
+    guid PRIMARY KEY,
+	title,
+   	link,
+    pubDate,
+    content,
+    contentSnippet
+)`).run();
 // getYnet().then((data) => writeFileSync("ynet.json", JSON.stringify(data)));
-(0, mako_1.getMako)().then((data) => (0, fs_1.writeFileSync)("mako.json", JSON.stringify(data)));
+// getMako().then((data) => writeFileSync("mako.json", JSON.stringify(data)));
 // getWalla().then((data) => writeFileSync("walla.json", JSON.stringify(data)));
