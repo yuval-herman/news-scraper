5e2c143580900bdfe4b254b16a9670ba50871da8
yuval <nhajeho1@gmail.com>
2022-09-29 21:01:22 +0300

    setup basic server with static serving and db fetching

---
+++ server/server.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import path from "path";
+import Database from "better-sqlite3";
+
+const isDeployed = Boolean(process.env.NEWS_SCRAPER_DEPLOYED);
+const dbPath = isDeployed
+	? path.join(__dirname, "../scraper/db.db")
+	: path.join(__dirname, "../../scraper/dist/db.db");
+
+const db = new Database(dbPath);
+const getArticleById = (id: string) =>
+	db.prepare("SELECT * FROM articles WHERE guid = ?").get(id);
+
+const app = express();
+const port = 4000;
+
+const STATIC_PATH = path.join(__dirname, "../../news-game/build");
+app.use(express.static(STATIC_PATH));
+
+app.get("/random/talkback/", (req, res) => {
+	const amount = req.query.amount || 1;
+	const talkbacks = db
+		.prepare(
+			"SELECT * FROM talkbacks WHERE parentID is NULL ORDER BY RANDOM() LIMIT " +
+				amount
+		)
+		.all();
+	res.json(talkbacks);
+});
+
+app.get("/random/article/", (req, res) => {
+	const amount = req.query.amount || 1;
+	const article = db
+		.prepare("SELECT * FROM articles ORDER BY RANDOM() LIMIT " + amount)
+		.all();
+	res.json(article);
+});
+
+app.get("/talkback/:id", (req, res) => {
+	const article = getArticleById(req.params.id);
+	if (!article) res.status(404).send("article not found");
+	else res.json(article);
+});
+
+app.get("*", (_, res) => res.sendFile(path.join(STATIC_PATH, "index.html")));
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
