f4cf92764677f24389af2ad8e83c8347584e93c1
yuval <nhajeho1@gmail.com>
2022-10-04 15:20:21 +0300

    added instructions and back to menu buttons

--- news-game/src/components/App/App.tsx
+++ news-game/src/components/App/App.tsx
@@ -3,13 +3,52 @@ import Game from "../Game/Game";
 import style from "./App.module.scss";
 
 enum GameState {
+	menu,
 	playing,
+	instructions,
 }
 
 function App() {
-	const [gameState, setGameState] = useState<GameState>();
+	const [gameState, setGameState] = useState<GameState>(GameState.menu);
 	if (gameState === GameState.playing) {
-		return <Game />;
+		return (
+			<div>
+				<button
+					style={{ margin: "1rem" }}
+					className={style.button}
+					onClick={() => setGameState(GameState.menu)}
+				>
+					חזרה לתפריט
+				</button>
+				<Game />
+			</div>
+		);
+	}
+	if (gameState === GameState.instructions) {
+		return (
+			<div className={style.main}>
+				<div className={style.instructions}>
+					<p>המשחק די פשוט!</p>
+					<p>במסך יוצגו 2 אלמנטים עיקריים: המאמר, והתגובות.</p>
+					<p>
+						עליכם לקרוא את המאמר ולנחש איזו תגובה מהתגובות שייכת במקור
+						למאמר.
+					</p>
+					<p>
+						<strong>שימו לב!</strong> המאמרים והתגובות כולם אמיתיים
+						ופורסמו באתרי חדשות שונים, אם ראיתם כתבה או תגובה ותהיתם מה
+						הסיפור המלא, לאחר לחיצה על התגובה שבחרתם יפיעו על התגובות
+						קישורים למאמרים המקוריים.
+					</p>
+					<button
+						className={style.button}
+						onClick={() => setGameState(GameState.menu)}
+					>
+						חזרה לתפריט
+					</button>
+				</div>
+			</div>
+		);
 	}
 	return (
 		<div className={style.main}>
@@ -21,7 +60,12 @@ function App() {
 				>
 					להתחלה!
 				</button>
-				<button className={style.button}>הוראות</button>
+				<button
+					className={style.button}
+					onClick={() => setGameState(GameState.instructions)}
+				>
+					הוראות
+				</button>
 			</div>
 		</div>
 	);
