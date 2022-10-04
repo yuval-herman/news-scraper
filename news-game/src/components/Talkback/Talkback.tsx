b159fabbf2bc1d7ce89afd54fa51097521c9abf1
yuval <nhajeho1@gmail.com>
2022-10-04 12:50:25 +0300

    added links to original articles on talkbacks

--- news-game/src/components/Talkback/Talkback.tsx
+++ news-game/src/components/Talkback/Talkback.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, useEffect, useState } from "react";
 import { DBTalkback } from "../../../../scraper/scraper";
 import style from "./Talkback.module.scss";
 
@@ -9,14 +10,29 @@ interface Props {
 }
 
 function Talkback(props: Props) {
+	const [articleLink, setArticleLink] = useState<string>();
+	useEffect(() => {
+		fetch(`/article/${talkback.articleGUID}`).then((r) =>
+			r.json().then((article) => setArticleLink(article.link))
+		);
+	});
+	function onClick(event: MouseEvent) {
+		if ((event.target as HTMLElement).nodeName === "A") return;
+		props.onClick();
+	}
 	const talkback = props.talkback;
 	let showCorrectClass = " ";
 	if (props.showCorrect) {
 		showCorrectClass += props.isCorrect ? style.correct : style.incorrect;
 	}
 	return (
-		<div onClick={props.onClick} className={style.main + showCorrectClass}>
-			<p className={style.writer}>{talkback.writer}</p>
+		<div onClick={onClick} className={style.main + showCorrectClass}>
+			<div className={style.head}>
+				<p className={style.writer}>{talkback.writer}</p>
+				<a href={articleLink} className={style.link} target="_blank">
+					Link to original article
+				</a>
+			</div>
 			<p className={talkback.content ? style.title : style.content}>
 				{talkback.title}
 			</p>
