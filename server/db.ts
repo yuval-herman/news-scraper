76c5966df1a891ac6e85d00cefe9163679ebf40f
yuval <nhajeho1@gmail.com>
2022-10-15 21:17:14 +0300

    added jsdoc to db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,47 +1,84 @@
 import path from "path";
 import Database from "better-sqlite3";
 
+// Db path changes after compilation, thus check if deployed to use new path
 const isDeployed = Boolean(process.env.NEWS_SCRAPER_DEPLOYED);
 const dbPath = isDeployed
 	? path.join(__dirname, "../scraper/db.db")
 	: path.join(__dirname, "../../scraper/dist/db.db");
 
 const db = new Database(dbPath);
-export const getArticleById = (id: string) =>
-	db.prepare("SELECT * FROM articles WHERE guid = ?").get(id);
+
+/**
+ * Get article by it's guid.
+ * @param guid article guid
+ */
+export const getArticleById = (guid: string) =>
+	db.prepare("SELECT * FROM articles WHERE guid = ?").get(guid);
+
+/**
+ * Get all talkbacks on an article by the article guid.
+ * @param guid article guid
+ */
 export const getTalkbacksByArticleGuid = (guid: string) =>
 	db
 		.prepare(
 			`SELECT * FROM talkbacks
 			WHERE articleGUID = ? and parentID is NULL;`
+			// Don't return talkbacks with parentID so that we won't
+			// get unclear text (a reply to another talkback)
 		)
 		.all(guid);
 
+/**
+ * Get all talkbacks related to topic array shuffled randomly.
+ * @param topics an array of topics to search for
+ */
 export const getTalkbacksByTopic = (topics: string[]) => {
 	const data: { mainTopic: string }[] = db
 		.prepare(
 			`SELECT * from talkbacks
-		where mainTopic != '[]' ORDER by random()`
+		     where mainTopic != '[]' ORDER by random()`
 		)
 		.all();
-	return data.filter((item) =>
-		(JSON.parse(item.mainTopic) as string[]).some((item) =>
-			topics.includes(item)
-		)
-	);
+	return data.filter((item) => {
+		const topics = JSON.parse(item.mainTopic) as string[];
+		return topics.some((item) => topics.includes(item));
+	});
 };
 
+/**
+ * Get a article by it's rowid in the db.
+ * @param id sqlite rowid
+ */
 export const getArticleByRowid = (id: string) =>
 	db.prepare("SELECT * FROM articles WHERE rowid = ?").get(id);
+
+/**
+ * Get all articles guid's in random order.
+ */
 export const getArticlesGuidRandomOrder = (): string[] => {
 	return db
 		.prepare(`SELECT guid from articles ORDER by random()`)
 		.pluck()
 		.all();
 };
+
+/**
+ * Get articles biggest rowid.
+ */
 export const articlesMaxRowid = () =>
 	db.prepare("SELECT max(rowid) FROM articles").get()["max(rowid)"];
+
+/**
+ * Get talkback by it's rowid.
+ * @param id talkback rowid
+ */
 export const getTalkbackByRowid = (id: string) =>
 	db.prepare("SELECT * FROM talkbacks WHERE rowid = ?").get(id);
+
+/**
+ * Get talkbacks biggest rowid.
+ */
 export const talkbacksMaxRowid = () =>
 	db.prepare("SELECT max(rowid) FROM talkbacks").get()["max(rowid)"];
