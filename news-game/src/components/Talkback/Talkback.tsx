e40334abc19d3802baa35d647bbfcf4a3beaa4c3
yuval <nhajeho1@gmail.com>
2022-10-19 14:19:40 +0300

    improved super long talkbacks handling

--- news-game/src/components/Talkback/Talkback.tsx
+++ news-game/src/components/Talkback/Talkback.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useEffect, useState } from "react";
+import React, { MouseEvent, useEffect, useState } from "react";
 import { DBTalkback } from "../../../../common/types";
 import style from "./Talkback.module.scss";
 
@@ -23,6 +23,9 @@ function Talkback(props: Props) {
 	function onClick(event: MouseEvent) {
 		// Ignore clicks on the article link.
 		if ((event.target as HTMLElement).nodeName === "A") return;
+		// Ignore clicks if showing answers.
+		if (props.showCorrect) return;
+
 		props.onClick();
 	}
 	let showCorrectClass = " ";
@@ -33,20 +36,34 @@ function Talkback(props: Props) {
 		<div onClick={onClick} className={style.main + showCorrectClass}>
 			<div className={style.head}>
 				<p className={style.writer}>{talkback.writer}</p>
-				<a
-					href={articleLink}
-					className={style.link}
-					rel="noreferrer"
-					target="_blank"
-				>
-					Link to original article
-				</a>
+				{props.showCorrect ? (
+					<a
+						href={articleLink}
+						className={style.link}
+						rel="noreferrer"
+						target="_blank"
+					>
+						Link to original article
+					</a>
+				) : undefined}
 			</div>
-			<p className={talkback.content ? style.title : style.content}>
-				{talkback.title}
-			</p>
+			{talkback.title ? (
+				<p className={talkback.content ? style.title : style.content}>
+					{talkback.title}
+				</p>
+			) : undefined}
 			{talkback.content ? (
-				<p className={style.content}>{talkback.content}</p>
+				<p
+					className={
+						style.content +
+						(talkback.content.length > 500 ||
+						(talkback.title && talkback.title?.length > 500)
+							? " " + style["content-long"]
+							: "")
+					}
+				>
+					{talkback.content}
+				</p>
 			) : undefined}
 		</div>
 	);
