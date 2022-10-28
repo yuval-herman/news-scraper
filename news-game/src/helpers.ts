e2c6499cb6dfc52f3d7dc6e27e2294821c01908b
yuval <nhajeho1@gmail.com>
2022-10-28 11:03:38 +0300

    added json fetch and post functions to helpers

--- news-game/src/helpers.ts
+++ news-game/src/helpers.ts
@@ -3,3 +3,35 @@ const DOM = new DOMParser();
 export function removeHTML(html: string) {
 	return DOM.parseFromString(html, "text/html").body.textContent;
 }
+
+/**
+ * An alias to (await fetch(input, init)).json()
+ * @param input url to fetch
+ * @param init fetch options
+ */
+export async function jsonFetch(input: RequestInfo | URL, init?: RequestInit) {
+	return (await fetch(input, init)).json();
+}
+
+/**
+ * An alias to (await fetch(input, data, init)).json()
+ * @param input url to fetch
+ * @param data data to send
+ * @param init fetch options
+ */
+export async function jsonPost(
+	input: RequestInfo | URL,
+	data: unknown,
+	init?: RequestInit
+) {
+	return await (
+		await fetch(input, {
+			...init,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+	).json();
+}
