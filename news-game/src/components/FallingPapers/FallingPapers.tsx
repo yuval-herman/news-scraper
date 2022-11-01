a299c1d0774d455d1939e443c00ba064b466e344
yuval <nhajeho1@gmail.com>
2022-11-01 08:57:46 +0200

    optimized papers as well as added variable speeds

--- news-game/src/components/FallingPapers/FallingPapers.tsx
+++ news-game/src/components/FallingPapers/FallingPapers.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useRef } from "react";
-import style from "./FallingPapers.module.scss";
+import { memo, useEffect, useRef } from "react";
 import newspaper1 from "../../assets/newspaper1.svg";
 import newspaper2 from "../../assets/newspaper2.svg";
 import newspaper3 from "../../assets/newspaper3.svg";
 import newspaper4 from "../../assets/newspaper4.svg";
 import newspaper5 from "../../assets/newspaper5.svg";
+import style from "./FallingPapers.module.scss";
 
 const papers = [newspaper1, newspaper2, newspaper3, newspaper4, newspaper5];
 
-export default function FallingPapers(props: { amount?: number }) {
+function FallingPapers(props: { amount?: number }) {
 	const papersRef = useRef<HTMLImageElement[]>([]);
 	const handler = (idx: number) => (e: React.FormEvent<HTMLImageElement>) => {
 		const next = papersRef.current[idx + 1];
@@ -19,25 +19,32 @@ export default function FallingPapers(props: { amount?: number }) {
 	useEffect(() => {
 		if (!papersRef.current[0].style.top) {
 			for (let i = 0; i < papersRef.current.length; i++) {
-				papersRef.current[i].style.top = Math.random() * 200 - 200 + "vh";
-				papersRef.current[i].style.left = Math.random() * 100 + "vw";
+				const x = Math.random() * 100 + "vw";
+				const y = Math.round(Math.random() * 200 - 200) + "vh";
+				papersRef.current[i].style.translate = x + " " + y;
 				papersRef.current[i].style.rotate = Math.random() * 360 + "deg";
 			}
 		}
 		const papersInterval = setInterval(() => {
 			for (let i = 0; i < papersRef.current.length; i++) {
-				let loc: number =
-					parseFloat(papersRef.current[i].style.top || "0") + 10;
+				// Get previous [x,y] coordinates
+				const paperSpeed = parseFloat(
+					papersRef.current[i].getAttribute("paper-speed")!
+				);
+				let [x, y] = papersRef.current[i].style.translate
+					.split(" ")
+					.map((value) => parseFloat(value || "0")) as [number, number];
+				y += paperSpeed * 5 + 5;
 
-				if (loc >= 110) {
-					loc = -20;
+				if (y >= 110) {
+					y = -20;
 					papersRef.current[i].style.display = "none";
 					papersRef.current[i].style.rotate = Math.random() * 360 + "deg";
-					papersRef.current[i].style.left = Math.random() * 100 + "vw";
+					x = Math.random() * 100;
 				} else {
 					papersRef.current[i].style.display = "unset";
 				}
-				papersRef.current[i].style.top = loc + "vh";
+				papersRef.current[i].style.translate = `${x}vw ${y}vh`;
 			}
 		}, 1000);
 		return () => clearInterval(papersInterval);
@@ -53,6 +60,7 @@ export default function FallingPapers(props: { amount?: number }) {
 						className={style.paper}
 						src={papers[i % papers.length]}
 						ref={(el) => (papersRef.current[i] = el!)}
+						paper-speed={Math.random()}
 						alt=""
 						onChange={handler(i)}
 					/>
@@ -60,3 +68,5 @@ export default function FallingPapers(props: { amount?: number }) {
 		</div>
 	);
 }
+
+export default memo(FallingPapers);
