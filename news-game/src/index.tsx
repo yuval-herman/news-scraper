f29ba72dcf226afe933ba4a9cb75844850c4398f
yuval <nhajeho1@gmail.com>
2022-11-01 15:01:48 +0200

    added register for service worker in index.tsx

--- news-game/src/index.tsx
+++ news-game/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.scss";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -11,3 +12,4 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
+serviceWorkerRegistration.register();
